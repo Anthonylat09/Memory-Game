057df2e14aeb6153cd8ce029757fd2988057b14f
anthonylat09 <antoinendiaye09@gmail.com>
2022-06-14 14:11:18 +0200

    feat: rendering cards with component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <h1>Memory</h1>
   <section class="game-board">
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
+    <CardComponent 
+      v-for="(card, index) in cardList" 
+      :key="`card-${index}`" 
+      :value="card"/>
   </section>
 </template>
 
 <script>
+import CardComponent from './components/CardComponent'
 
 export default {
   name: 'App',
+  components: {
+    CardComponent
+  },
+  setup(){
+    const cardList = []
+
+    for(let i = 0; i < 16; i++){
+      cardList.push(i)
+    }
+
+    return {
+      cardList
+    }
+  }
 }
 </script>
 
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+
+}
+
+</script>
+
+<template>
+    <div class="card">
+        Card
+    </div>
+</template>
