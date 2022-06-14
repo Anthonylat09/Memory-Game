7f0a81b4a078bffc84a96dd6deff291fd1308e24
anthonylat09 <antoinendiaye09@gmail.com>
2022-06-14 20:40:12 +0200

    feat: flipping cards front and back

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,16 @@
     <CardComponent 
       v-for="(card, index) in cardList" 
       :key="`card-${index}`" 
-      :value="card"
+      :value="card.value"
+      :visible="card.visible"
+      :position="card.position"
+      @select-card="flipCard"
     />
   </section>
 </template>
 
 <script>
+import { ref } from 'vue'
 import CardComponent from './components/CardComponent'
 
 export default {
@@ -18,14 +22,23 @@ export default {
     CardComponent
   },
   setup(){
-    const cardList = []
+    const cardList = ref([])
 
     for(let i = 0; i < 16; i++){
-      cardList.push(i)
+      cardList.value.push({
+        value: i,
+        visible: false,
+        position: i
+      })
+    }
+
+    const flipCard = (payload) => {
+      cardList.value[payload.position].visible = true
     }
 
     return {
-      cardList
+      cardList,
+      flipCard
     }
   }
 }
